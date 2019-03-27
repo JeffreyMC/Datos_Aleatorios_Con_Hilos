445a521009cce28c74c9201c447016101c1e79c2
Jeffrey <jmunozcastro16@gmail.com>
2019-03-26 23:40:23 -0600

    Números aleatorios por medio de hilos

---
+++ Tarea3/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Tarea3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLlenaArray = new System.Windows.Forms.Button();
+            this.btnInicProcesos = new System.Windows.Forms.Button();
+            this.campoTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLlenaArray
+            // 
+            this.btnLlenaArray.Location = new System.Drawing.Point(22, 30);
+            this.btnLlenaArray.Name = "btnLlenaArray";
+            this.btnLlenaArray.Size = new System.Drawing.Size(128, 46);
+            this.btnLlenaArray.TabIndex = 0;
+            this.btnLlenaArray.Text = "Llenar Arreglo";
+            this.btnLlenaArray.UseVisualStyleBackColor = true;
+            this.btnLlenaArray.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnInicProcesos
+            // 
+            this.btnInicProcesos.Location = new System.Drawing.Point(191, 30);
+            this.btnInicProcesos.Name = "btnInicProcesos";
+            this.btnInicProcesos.Size = new System.Drawing.Size(128, 46);
+            this.btnInicProcesos.TabIndex = 1;
+            this.btnInicProcesos.Text = "Iniciar subprocesos";
+            this.btnInicProcesos.UseVisualStyleBackColor = true;
+            this.btnInicProcesos.Click += new System.EventHandler(this.boton2_Click);
+            // 
+            // campoTexto
+            // 
+            this.campoTexto.Location = new System.Drawing.Point(22, 139);
+            this.campoTexto.Multiline = true;
+            this.campoTexto.Name = "campoTexto";
+            this.campoTexto.ReadOnly = true;
+            this.campoTexto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.campoTexto.Size = new System.Drawing.Size(297, 213);
+            this.campoTexto.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Posición";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(246, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 364);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.campoTexto);
+            this.Controls.Add(this.btnInicProcesos);
+            this.Controls.Add(this.btnLlenaArray);
+            this.Name = "Form1";
+            this.Text = "Tarea 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLlenaArray;
+        private System.Windows.Forms.Button btnInicProcesos;
+        private System.Windows.Forms.TextBox campoTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Tarea3/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; //nombre de espacios para manejar hilos
+
+//Tarea 3
+//Programación Avanzada
+//Estudiante: Jeffrey Muñoz Castro
+//Cédula: 1-1370-0324
+namespace Tarea3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //se crea un arreglo int de 50 posiciones
+        int[] arreglo = new int[50];
+
+        //se crea un objeto random para generar los números aleatorios del arreglo
+        //y para generar los numeros que recorre cada proceso
+        //se le ingresa una semilla con el tiempo de la computadora
+        Random rnd = new Random(DateTime.Now.Millisecond);
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //desabilita el botón que inicia los procesos
+            btnInicProcesos.Enabled = false;
+        }
+
+        //Botón que al presionarse llena el arreglo con numeros
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //se llena el arreglo con numeros random del 1 al 256
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = rnd.Next(1, 256);
+            }
+
+            //habilita el botón que inicia los procesos
+            btnInicProcesos.Enabled = true;
+            //se desactiva el botón de llenar arreglo
+            btnLlenaArray.Enabled = false;
+
+            //limpia el texbox (en caso de que esté con información)
+            campoTexto.Clear();
+        }
+
+        private void boton2_Click(object sender, EventArgs e)
+        {
+            //se crean los 5 subprocesos
+            Thread hilo1 = new Thread(new ParameterizedThreadStart(imprimir));
+            hilo1.Name = "Proceso 1"; //se le asigna el nombre al hilo
+            Thread hilo2 = new Thread(new ParameterizedThreadStart(imprimir));
+            hilo2.Name = "Proceso 2"; //se le asigna el nombre al hilo
+            Thread hilo3 = new Thread(new ParameterizedThreadStart(imprimir));
+            hilo3.Name = "Proceso 3"; //se le asigna el nombre al hilo
+            Thread hilo4 = new Thread(new ParameterizedThreadStart(imprimir));
+            hilo4.Name = "Proceso 4"; //se le asigna el nombre al hilo
+            Thread hilo5 = new Thread(new ParameterizedThreadStart(imprimir));
+            hilo5.Name = "Proceso 5"; //se le asigna el nombre al hilo
+
+            //se les asigna la prioridad a cada subproceso
+            hilo1.Priority = ThreadPriority.Lowest;
+            hilo2.Priority = ThreadPriority.Lowest;
+            hilo3.Priority = ThreadPriority.Highest;
+            hilo4.Priority = ThreadPriority.Highest;
+            hilo5.Priority = ThreadPriority.Highest;
+
+
+            //se inician los 5 subprocesos y se les pasa de argumento el nombre del hilo
+            hilo1.Start(hilo1.Name);
+            hilo2.Start(hilo2.Name);
+            hilo3.Start(hilo3.Name);
+            hilo4.Start(hilo4.Name);
+            hilo5.Start(hilo5.Name);
+
+            //desactiva el boton que inicia los procesos
+            btnInicProcesos.Enabled = false;
+            //habilita el botón que llena el arreglo
+            btnLlenaArray.Enabled = true;
+        }
+
+        //se crea el delegado para manejar los subprocesos
+        public delegate void delegadoImprimir(string name);
+
+        //método que concatena la posicion, valor y nombre del subproceso
+        public void imprimir(object name)
+        {
+            //si modificar el campoTexto es inseguro
+            if (campoTexto.InvokeRequired)
+            {
+                  //se utiliza invoke para cambiar campoTexto por medio del delegado
+                  Invoke(new delegadoImprimir(imprimir), new object[] { name });
+             }
+            //si se puede modificar
+            else
+            {
+                //se recorren 10 posiciones del arreglo
+                for (int i = 0; i < 10; i++)
+                {
+                    //genera un numero del 0-49 y lo almacena
+                    int num = rnd.Next(0, 49);
+
+                    //se concatena la información
+                    campoTexto.AppendText(string.Format("{0}\t\t{1, 11}\t\t{2, 18}\n", num, arreglo[num], name ));
+                    //Se 'duerme' el hilo por 100 milésimas de segundo
+                    Thread.Sleep(100);
+
+                }//fIN DEL FOR
+            }//FIN DEL ELSE
+
+        }//FIN DEL MÉTODO IMPRIMIR
+
+
+    }// FIN DE LA CLASE
+
+
+  }
+    
+
